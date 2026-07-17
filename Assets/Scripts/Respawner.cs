ba78e10b867f87253cb910d41d1fadd20e8a95b6
Jonas Tamimi <oas@users.noreply.github.com>
2026-07-17 14:48:20 +0200

    Initial commit

--- Assets/Scripts/Respawner.cs
+++ Assets/Scripts/Respawner.cs
@@ -4,40 +4,20 @@ using UnityEngine;
 
 public class Respawner : MonoBehaviour
 {
-    [SerializeField]
-    GameObject spieler_prefab;
-
-    [SerializeField]
-    GameObject spieler_active;
-
-
-    // f steht für float
+    // f steht fÃ¼r float
     [SerializeField]
     float y_spawn_pos = 1f;
     [SerializeField]
     float x_spawn_pos = -8f;
 
-
-    void SpawnSpieler()
-    {
-        Vector2 position = new Vector2(x_spawn_pos, y_spawn_pos);
-        spieler_active = Instantiate(spieler_prefab, position, Quaternion.identity);
-    }
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-        SpawnSpieler();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (spieler_active == null) {
-            SpawnSpieler();
-        }
 
     }
 }
